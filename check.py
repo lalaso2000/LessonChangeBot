d0d1c6088e902058a489647709decb0f3381e950
Lalaso2000 <raraso2000@icloud.com>
2018-03-12 17:24:04 +0900

    【修正】リファクタリング

--- check.py
+++ check.py
@@ -5,43 +5,37 @@ import lessonData
 import datetime
 import logging.config
 
+# ロガー設定
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger()
 
-def main():
-    # ロガー設定
-    logging.config.fileConfig('logging.conf')
-    logger = logging.getLogger()
+# 授業変更をチェックする
+# update = lessonData.updateCheck()
+update = lessonData.updateCheck(
+    url='https://www.dropbox.com/s/p6hlhjp5f5v3y50/keijiyou.pdf?dl=1',
+    file_path='test.pdf')  # デバック用
+if update:
+    # cd = lessonData.get_data(False)
+    cd = lessonData.get_data(
+        False, pdf_path='test.pdf', csv_path='test.csv')  # デバック用
+else:
+    # cd = lessonData.get_data(True)
+    cd = lessonData.get_data(True, csv_path='test.csv')  # デバック用
+# print(cd)
 
-    # 授業変更をチェックする
-    # update = lessonData.updateCheck()
-    update = lessonData.updateCheck(
-        url='https://www.dropbox.com/s/p6hlhjp5f5v3y50/keijiyou.pdf?dl=1',
-        file_path='test.pdf')  # デバック用
-    if update:
-        # cd = lessonData.get_data(False)
-        cd = lessonData.get_data(
-            False, pdf_path='test.pdf', csv_path='test.csv')  # デバック用
-    else:
-        # cd = lessonData.get_data(True)
-        cd = lessonData.get_data(True, csv_path='test.csv')  # デバック用
-    # print(cd)
+# 5Eの授業変更を取り出す
+data_5e = lessonData.search_for_class(cd, grade=5, department='E')
+# print(data_5e)
 
-    # 5Eの授業変更を取り出す
-    data_5e = lessonData.search_for_class(cd, grade=5, department='E')
-    # print(data_5e)
+# 明日の授業変更を探す
+today = datetime.date.today()
+# today = datetime.date(2018, 3, 11)  # デバック用
+td = datetime.timedelta(days=1)
+tomorrow = today + td
+# print(tomorrow)
+data_5e_tomorrow = lessonData.search_for_date(data_5e, tomorrow)
+# print(data_5e_tomorrow)
+logger.log(20, '検索結果 \n {}'.format(data_5e_tomorrow))
 
-    # 明日の授業変更を探す
-    # today = datetime.date.today()
-    today = datetime.date(2018, 3, 11)  # デバック用
-    td = datetime.timedelta(days=1)
-    tomorrow = today + td
-    # print(tomorrow)
-    data_5e_tomorrow = lessonData.search_for_date(data_5e, tomorrow)
-    # print(data_5e_tomorrow)
-    logger.log(20, '検索結果 \n {}'.format(data_5e_tomorrow))
-
-    # csv出力しておく
-    data_5e_tomorrow.to_csv('tomorrow.csv', encoding='utf-8')
-
-
-if __name__ == '__main__':
-    main()
+# csv出力しておく
+data_5e_tomorrow.to_csv('tomorrow.csv', encoding='utf-8')
--- line.py
+++ line.py
@@ -4,72 +4,21 @@
 import lessonData
 import logging.config
 import pandas as pd
-import requests
-import time
+import lineNotify
 
 # ロガー設定
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger()
 
+# 明日の授業変更を持ってくる
+data_5e_tomorrow = pd.read_csv('tomorrow.csv')
 
-def get_token(token_file_path='line_token.txt'):
-    """トークンを取得
+# メッセージを作る
+msgs = []
+for index, d in data_5e_tomorrow.iterrows():
+    msgs.append(lessonData.create_tweet(d))
+# print(msgs)
 
-    Keyword Arguments:
-        token_file_path {str} -- トークンが書かれたtxtファイル (default: {'line_token.txt'})
-
-    Returns:
-        list -- トークンの配列
-    """
-
-    tokens = []
-    with open(token_file_path) as f:
-        for line in f:
-            line = line.rstrip('\r\n')
-            tokens.append(line)
-    return tokens
-
-
-def post(token, msg):
-    for i in range(1, 4):
-        try:
-            # ポスト
-            headers = {
-                'Authorization': 'Bearer {}'.format(token),
-            }
-            files = {
-                'message': (None, msg),
-            }
-            response = requests.post(
-                'https://notify-api.line.me/api/notify',
-                headers=headers,
-                files=files)
-        except Exception as e:
-            logger.log(30, 'ポストに失敗しました。リトライします。({}/3)'.format(i))
-            time.sleep(i * 5)
-        else:
-            logger.log(20, 'response={}'.format(response))
-            return response
-    logger.log(40, 'ポストに失敗しました。')
-    return None
-
-
-def main():
-    # 明日の授業変更を持ってくる
-    data_5e_tomorrow = pd.read_csv('tomorrow.csv')
-
-    # メッセージを作る
-    msgs = []
-    for index, d in data_5e_tomorrow.iterrows():
-        msgs.append(lessonData.create_tweet(d))
-    # print(msgs)
-
-    # line Notifyにポスト
-    tokens = get_token()
-    for m in msgs:
-        for t in tokens:
-            post(t, m)
-
-
-if __name__ == '__main__':
-    main()
+# line Notifyにポスト
+ln = lineNotify.LineNotify()
+ln.multi_post(msgs)
